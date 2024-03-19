5b5985d30e7bbc0ef30b5efc0412284c6cc2ad88
aristidei <a.isingizwe@alustudent.com>
2024-03-19 12:45:36 +0200

    user comment conv mess post

---
+++ models/post.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema(
+  {
+    authorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: [],
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+        default: [],
+      },
+    ],
+    image: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+export default Post;
