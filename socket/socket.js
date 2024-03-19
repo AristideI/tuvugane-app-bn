7f98f4214eb10263c00aa677338829de3b5f13d8
aristidei <a.isingizwe@alustudent.com>
2024-03-19 23:29:29 +0200

    get socket online users

--- socket/socket.js
+++ socket/socket.js
@@ -11,11 +11,21 @@ export const io = new Server(server, {
   },
 });
 
-export const getRecievedSocketId = (recieverId) => {};
+const currentlyOnlineUsers = {};
+
+export const getRecievedSocketId = (recieverId) => {
+  return currentlyOnlineUsers[recieverId];
+};
 
 io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  currentlyOnlineUsers[userId] = socket.id
+  io.emit("currentlyOnlineUsers", currentlyOnlineUsers);
+
   console.log("a user connected");
   socket.on("disconnect", () => {
+    delete currentlyOnlineUsers[userId];
+    io.emit("currentlyOnlineUsers", currentlyOnlineUsers);
     console.log("user disconnected");
   });
 });
